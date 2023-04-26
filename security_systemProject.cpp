fb5567ca04121a4db35b12c5ec6d03f72a085879
dakshnitish <dakshnitish@gmail.com>
2023-04-26 18:09:03 +0530

    project of security system

---
+++ security_systemProject.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+using namespace std;
+int main(){
+    int a ,i=0;
+    string text ,old,password1,password2,pass,name,password0,age,user,word,word1;
+    string cred[2] ,cp[2];
+    cout<<"     security system"<<endl;
+    cout<<"_____________________________"<<endl;
+    cout<<"|       1. Register         |"<<endl;
+    cout<<"|       2. Login            |"<<endl;
+    cout<<"|       3. Change password  |"<<endl;
+    cout<<"|_______4. End program______|"<<endl;
+  return 0;
+}
